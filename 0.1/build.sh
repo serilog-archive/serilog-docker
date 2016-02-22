d0993f2852c26705ab313b2f48d2183b9f0b0229
Matthew Erbs <merbs@splunk.com>
2016-02-22 13:14:37 +1000

    Removed old sh file

--- 0.1/build.sh
+++
@@ -1,6 +0,0 @@
-for path in src/*/project.json; do
-  dirname="$(dirname "${path}")"
-  dnu restore ${dirname}
-  dnu build ${dirname} --configuration Release --out .\artifacts\testbin;
-  dnu pack ${dirname} --configuration Release --out .\artifacts\packages;
-done
