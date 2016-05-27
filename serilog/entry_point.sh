e84c99fe9eb28988e5703da18c52a96a8148bfd0
Matthew Erbs <matthew.erbs@merbla.com>
2016-05-27 19:12:44 +1000

    Moved to use build.sh file in repo.

--- serilog/entry_point.sh
+++ serilog/entry_point.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
-
+sudo https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.sh
 rm serilog
 git clone -b $SERILOG_BRANCH $SERILOG_REPO
 
 cd serilog/
-dotnet restore
-
-cd src/Serilog/
-
-dotnet build -f netstandard1.0 -c Release
-dotnet build -f netstandard1.3 -c Release
-
-cd ../..
-cd test/Serilog.Tests/ 
-
-dotnet build -f netcoreapp1.0 -c Release
-dotnet test -f netcoreapp1.0  -c Release
+sh build.sh
